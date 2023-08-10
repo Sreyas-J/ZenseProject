886a1648fb355ad06c31197cbd750e9084d69a32
Sreyas-J <Sreyas.Janamanchi@iiitb.ac.in>
2023-08-10 15:53:43 +0530

    user login,signup

--- ZenseProject/videoCall/urls.py
+++ ZenseProject/videoCall/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.home,name='home'),
+    path('',views.loginPage,name='login'),
+    path('home/',views.home,name='home'),
     path('room/',views.room,name='room'),
     path('get_token/',views.Token,name='token'),
     path('signup/',views.signup,name='signup'),
--- ZenseProject/videoCall/views.py
+++ ZenseProject/videoCall/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import JsonResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
 from agora_token_builder import RtcTokenBuilder
 import random
 import time
@@ -13,16 +18,46 @@ def Token(request):
     timeStamp=time.time()
     privilegeExpiredTs=timeStamp+expirationTime
     role=1
-    print(Id,certificate,channel,uid,expirationTime,timeStamp,privilegeExpiredTs,role)
+    #print(Id,certificate,channel,uid,expirationTime,timeStamp,privilegeExpiredTs,role)
 
     token = RtcTokenBuilder.buildTokenWithUid(Id, certificate, channel, uid, role, privilegeExpiredTs)
     return JsonResponse({'token':token,'uid':uid},safe=False)
 
+@login_required(login_url='/signup/') 
 def home(request):
     return render(request,'home.html')
 
+@login_required(login_url='/signup/') 
 def room(request):
     return render(request,'room.html')
 
 def signup(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        try:
+            instance=User.objects.get(username=username)
+            messages.error(request,'This user already exists. ')
+            return redirect('signup')
+        except:
+            password=request.POST.get('password')
+            user=User.objects.create(username=username,password=password)
+            login(request,user)
+            return redirect('home')
+
     return render(request,'signup.html')
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Redirect to the profile or desired page after successful login
+        else:
+            # User authentication failed
+            return render(request, 'login.html', {'error_message': 'Invalid credentials.'})
+    
+    return render(request, 'login.html')
